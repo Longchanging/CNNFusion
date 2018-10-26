55b291618a2cee54d21bf8d4ff4ef49ea81d2e01
MapleYlq <1280906473@qq.com>
2018-10-26 14:21:37 +0800

    Add baseline using RF
    Add baseline using RF

---
+++ src/baseline.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+'''
+@time:    Created on  2018-10-26 13:37:35
+@author:  Lanqing
+@Func:    Set baseline for the learning process before using CNN
+'''
+
+from config import tmp_path_base
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np, pandas as pd
+
+new_array = np.load(tmp_path_base + 'data.npy',)
+list_all_label = np.load(tmp_path_base + 'label.npy')
+
+idx = np.random.randint(new_array.shape[0], size=10000)
+print(idx)
+new_array = new_array[idx]
+list_all_label = list_all_label[idx]
+
+data = new_array.reshape([new_array.shape[0], new_array.shape[1] * new_array.shape[2]])
+le = LabelEncoder()
+label = le.fit_transform(list_all_label)
+df = pd.DataFrame(data)
+df = df.fillna(method='ffill')
+data = df.values
+
+data = MinMaxScaler().fit_transform(data)
+
+model2 = RandomForestClassifier(n_estimators=200)
+scores2 = cross_val_score(model2, data, label, cv=2, scoring='accuracy')
+print(scores2)
